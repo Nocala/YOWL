196be77ea72d8ea6b238259a0b3dced94fce29d6
Nocala <loan.lucmau@epitech.digital>
2025-02-05 18:24:17 +0100

    [UPD]{FRONT} Implement event creation form with image upload and state management

--- Go/app/(main)/events.jsx
+++ Go/app/(main)/events.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image, } from 'react-native';
 import { useRouter } from "expo-router";
 
 import { theme } from '../../constants/theme';
